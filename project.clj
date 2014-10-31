38b3d1025b452b713135c099a47730ef2b62d76f
Adrian Medina <adrian.medina@mail.yu.edu>
2014-10-30 20:46:48 -0400

    init

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject kinect "0.1.0-SNAPSHOT"
+  :description ""
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [com.cognitect/transit-clj "0.8.259"]
+                 [aleph "0.4.0-alpha7"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
+                   :source-paths ["dev"]}})
