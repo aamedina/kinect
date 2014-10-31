ad26bf0ef3fcdd7170e5eb89e05eeaeafe7e3d3e
Adrian Medina <adrian.medina@mail.yu.edu>
2014-10-30 21:42:23 -0400

    add macosx native lib for opencv

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.cognitect/transit-clj "0.8.259"]
+                 [org.usb4java/usb4java "1.2.0"]
                  [aleph "0.4.0-alpha7"]]
+  :jvm-opts ["-server" "-Djava.library.path=native/macosx"]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
                    :source-paths ["dev"]}})
